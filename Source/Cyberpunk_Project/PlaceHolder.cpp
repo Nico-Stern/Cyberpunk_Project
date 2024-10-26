e4fe63d489ef8c3f9cadb85e7fac754195e5c56c
Nico-Stern <nstern2002@gmail.com>
2024-10-26 20:27:15 +0200

    little stuff

---
+++ Source/Cyberpunk_Project/PlaceHolder.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaceHolder.h"
+
+PlaceHolder::PlaceHolder()
+{
+}
+
+PlaceHolder::~PlaceHolder()
+{
+}
